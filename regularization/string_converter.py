ffc5a20bd47bd79584cb40b74edc05fd5b38a361
Aruan <ruanhp1@lenovo.com>
2018-05-21 10:58:00 +0800

    add full2half

--- regularization/string_converter.py
+++ regularization/string_converter.py
@@ -15,8 +15,6 @@ class StringConverter(object):
         u"“": u'"',
         u"‘": u"'",
         u"’": u"'",
-        u"！": u"!",
-        u"。": u".",
         u"【": u"[",
         u"】": u"]",
         u"：": u":",
@@ -25,23 +23,28 @@ class StringConverter(object):
         u"（": u"(",
         u"）": u")",
         u"；": u";",
-        u"，": u","
+        # u"。": u".",
+
+        # negligible
+        # u"，": u","
+        # u"！": u"!",
+        # u"？": u"?",
     }
 
     @staticmethod
     def full_to_half(string):
         """全角转半角"""
-        rstring = ""
+        rstring = u""
         for uchar in string:
-            inside_code=ord(uchar)
+            inside_code = ord(uchar)
             if inside_code == 12288:
                 # 全角空格直接转换
                 char = unichr(32)
+            elif StringConverter.full_half_dict.get(uchar):
+                char = StringConverter.full_half_dict.get(uchar)
             elif 65281 <= inside_code <= 65374:
                 # 全角字符（除空格）根据关系转化
                 char = unichr(inside_code-65248)
-            elif StringConverter.full_half_dict.get(uchar):
-                char = StringConverter.full_half_dict.get(uchar)
             else:
                 char = uchar
 
@@ -53,7 +56,7 @@ class StringConverter(object):
         """半角转全角"""
         rstring = ""
         for uchar in string:
-            inside_code=ord(uchar)
+            inside_code = ord(uchar)
             if inside_code == 32:
                 # 半角空格直接转化
                 inside_code = 12288
@@ -67,7 +70,8 @@ class StringConverter(object):
 
 if __name__ == "__main__":
     pass
-    a = u"你好！今天气温36℃。他说：“Hello!”"
-    print a
-    print StringConverter.full_to_half(a)
-    print StringConverter.half_to_full(a)
+    text = u"你好！今天气温36℃。他说：“Hello!”２００１年在很多玩家的心中都留下了纯朴９：００－１８：００；" \
+        u"电话处理时间是７×２４当您可爱，哈哈！好吗？作为中国游戏市场上第一款３Ｄ网游qq产品。ＱＱ的,Ｍｏｒｅ！？。网游作品."
+    print text
+    print StringConverter.full_to_half(text)
+    # print StringConverter.half_to_full(text)
