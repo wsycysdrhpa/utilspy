298ebf2fec1d1b2bf0d7768480fdeb51a559544d
Aruan <ruanhp1@lenovo.com>
2018-08-02 16:00:43 +0800

    \.

--- text/en_preprocessor.py
+++ text/en_preprocessor.py
@@ -15,7 +15,7 @@ class EnPreprocessor(object):
     """
     def __init__(self, lower=True):
         self.lower = lower
-        self.re_useless = re.compile(u'[^0-9a-zA-Z\']')
+        self.re_useless = re.compile(u'[^0-9a-zA-Z\'\.]')
         self.re_muti_space = re.compile(u'\s+')
         # 处理 doctors' 这种情况，以下情况基本不会出现，可忽略，说话内容用单引号，结尾是名词复数的所有格
         # 忽略情形如: 'Yes, it is doctors''
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     pass
     en_preprocessor = EnPreprocessor(lower=True)
     # test_cent = u"##$Q!hello! man ,$!@you are!! 30 a baby's haha!~!!#my son's,moment's, Don't 5th 2st haha! "
-    test_cent = u"I've got it. 'Yes, ma'am.' the-waiter said.'好Yes, 3 is doctors'.' 'Yes, it is doctors'' he said."
+    test_cent = u"I've got it. 'Yes, ma'am.' the-waiter said.'好Yes, 3.4 is doctors'.' 'Yes, it is doctors'' he said."
     # test_cent = u"haha, \"www.qq@123.google.com\" tt2th 3st !"
 
     print test_cent
