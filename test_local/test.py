01d76e1f8c7884312a7a4307fdbf563f1c74280b
arain <ruanhaipeng@MacBook-Pro-7.local>
2026-04-20 18:06:47 +0800

    mod test

--- test_local/test.py
+++ test_local/test.py
@@ -9,3 +9,12 @@ class Test(object):
 
 if __name__ == "__main__":
     pass
+    import sys
+
+    from dotenv import load_dotenv
+    import project_root_finder
+
+    CURRENT_PROJECT_PATH = project_root_finder.root.as_posix()
+    sys.path.append(CURRENT_PROJECT_PATH)
+
+    load_dotenv()
